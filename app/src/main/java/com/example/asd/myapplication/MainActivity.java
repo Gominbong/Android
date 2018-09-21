ef42b95a965a9ad44a97813351a3ddedbe406dc7
Gominbong <sjy2017z@naver.com>
2018-09-22 00:34:08 +0900

    fff

--- app/src/main/java/com/example/asd/myapplication/MainActivity.java
+++ app/src/main/java/com/example/asd/myapplication/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.asd.myapplication;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
+
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button button1 = (Button)findViewById(R.id.button1);
         Button button2 = (Button)findViewById(R.id.button2);
-
+        Button button3 = (Button)findViewById(R.id.button3);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +44,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 getFragmentManager().beginTransaction().replace(R.id.framelayout, new FragmentActivity2()).commit();
+            }
+        });
+
+
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
             }
         });
     }
